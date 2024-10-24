2c5322b5a75bb93ec5ee233bcad8742dddf05a98
SokanRed <deltapi@sokan.fr>
2024-10-24 11:33:12 +0200

    Last updates

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,14 @@ export default function Page() {
         className="hidden md:block"
         alt="Screenshots of the dashboard project showing desktop version"
       />
-        </div>
+      <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing mobile version"
+      />
+          </div>
       </div>
     </main>
   );
